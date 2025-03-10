6096e88256a47863e4c27afdb6def4309389ea0f
Bryan Albert <bryan.albert@gmail.com>
2025-03-10 13:48:02 -0500

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,16 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
-
-function run() {
-  core.notice('Hello from my custom JavaScript Actions!');
-}
-
-run();
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-const core = require('@actions/core');
 // const github = require('@actions/github');
 const exec = require('@actions/exec');
 
@@ -24,10 +12,18 @@ function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-  core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
+  // Not using github object but note:
+
+  // Send to REST API
+  // github.getOctokit().rest
+
+  // Get access to the GitHub context
+  // github.context.
+
+  //const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  //core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
+
+  //core.notice('Hello from my custom JavaScript Actions!');
 }
 
 run();
-*/
-
